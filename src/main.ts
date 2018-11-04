cdba1e4a1d9e1829754f843f6ab9e46c47bec53e
Jukka Lähetkangas <jukka.lahetkangas@fraktio.fi>
2018-11-04 17:19:34 +0200

    stretch full screen

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { HUD } from './scenes/HUD'
 import { MainScene } from './scenes/mainScene'
 // main game configuration
 const config: GameConfig = {
-  width: 800,
-  height: 600,
+  width: 1024,
+  height: 768,
   type: Phaser.AUTO,
   parent: 'game',
   scene: [MainScene, HUD],
@@ -13,7 +13,6 @@ const config: GameConfig = {
   audio: {
     disableWebAudio: true,
   },
-   
 }
 
 // game class
@@ -24,5 +23,24 @@ export class Game extends Phaser.Game {
 }
 
 // when the page is loaded, create our game instance
-new Game(config)
+function resize(game) {
+  var canvas = document.querySelector('canvas')
+  var windowWidth = window.innerWidth
+  var windowHeight = window.innerHeight
+  var windowRatio = windowWidth / windowHeight
+  // @ts-ignore
+  var gameRatio = game.config.width / game.config.height
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + 'px'
+    canvas.style.height = (windowWidth / gameRatio) + 'px'
+  } else {
+    canvas.style.width = (windowHeight * gameRatio) + 'px'
+    canvas.style.height = windowHeight + 'px'
+  }
+}
+
+window.onload = () => {
+  const game = new Game(config)
+  resize(game)
+}
 // */
