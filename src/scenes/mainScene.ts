bb45291132294b90e1ae500aa4bb501a759130c4
rapuhotelli <jukka@lahetkangas.net>
2018-11-05 22:19:47 +0200

    cursor

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -4,12 +4,17 @@ const minusEighth = (number: number) => number - number/8
 
 export class MainScene extends Phaser.Scene {
     // private phaserSprite: Phaser.GameObjects.Sprite;
+  private debugText: Phaser.GameObjects.Text
+  private gridMap: Phaser.Tilemaps.Tilemap
+  private cursorPosition: {x: number, y: number}
+  private gridCursor: Phaser.GameObjects.Graphics
 
   constructor() {
     super({
       key: 'MainScene',
       active: true,
     })
+    this.cursorPosition = {x: 0, y: 0}
   }
 
   preload(): void {
@@ -20,7 +25,6 @@ export class MainScene extends Phaser.Scene {
 
   create(): void {
     const levelGraphic = this.add.image(512, 384, 'levelGraphic')
-
     levelGraphic.displayHeight = levelGraphic.height - levelGraphic.height/8 // 640
     const GRID_WIDTH = 32
     const GRID_HEIGHT = 24 // 24
@@ -30,8 +34,33 @@ export class MainScene extends Phaser.Scene {
       gridTileMap[y] = Array(GRID_WIDTH).fill(0)
     }
 
-    const map = this.make.tilemap({ data: gridTileMap, tileWidth: 32, tileHeight: 32 - (32/8) })
-    const tiles = map.addTilesetImage('gridSquare')
-    const layer = map.createStaticLayer(0, tiles, 0, levelGraphic.height/8/2)
+    this.gridMap = this.make.tilemap({ data: gridTileMap, tileWidth: 32, tileHeight: 32 - (32/8) })
+    const tiles = this.gridMap.addTilesetImage('gridSquare')
+    const layer = this.gridMap.createStaticLayer(0, tiles, 0, levelGraphic.height/8/2)
+
+
+    this.debugText = this.add.text(50, 50, 'null')
+    console.log(this.input.activePointer.positionToCamera(this.cameras.main))
+
+    this.gridCursor = this.add.graphics({ lineStyle: { width: 2, color: 0x000000, alpha: 1 } })
+    this.gridCursor.strokeRect(0, 0, 32, 32 - (32/8))
+    this.gridCursor.setPosition(0, 0)
+
+  }
+  update(): void {
+    this.input.activePointer.positionToCamera(this.cameras.main, this.cursorPosition)
+    // this.debugText.setText(`x:${this.cursorPosition.x} y:${this.cursorPosition.y}`)
+    const cursorTile = this.gridMap.getTileAtWorldXY(this.cursorPosition.x, this.cursorPosition.y)
+
+    if (cursorTile) {
+      if (!this.gridCursor.visible) {
+        this.gridCursor.setVisible(true)
+      }
+      this.gridCursor.setPosition(cursorTile.getLeft(), cursorTile.getTop())
+    } else {
+      if (this.gridCursor.visible) {
+        this.gridCursor.setVisible(false)
+      }
+    }
   }
 }
